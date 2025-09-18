e9c9146380d29573e1710d7bf3b4974893336c04
sjmy <23250749+sjmy@users.noreply.github.com>
2025-09-17 21:32:16 -0700

    Education and work experience state updating and displaying on change

--- src/components/EducationInput.jsx
+++ src/components/EducationInput.jsx
@@ -1,91 +1,190 @@
 import "../styles/EducationInput.css";
 
-function SchoolInput({ education, onEducationChange }) {
+function SchoolInput({ entry, education, onEducationChange }) {
   return (
-    <div className="schoolContainerInput">
+    <div className="schoolInputContainer">
       <label htmlFor="school">School: </label>
       <input
         className="schoolInput"
         type="text"
-        value={education.school}
+        value={entry.school}
         placeholder="School Name"
         id="school"
         autoComplete="off"
         onChange={(e) =>
-          handleSchoolChange(education, onEducationChange, e.target.value)
+          handleSchoolChange(
+            entry,
+            education,
+            onEducationChange,
+            e.target.value
+          )
         }
       />
     </div>
   );
 }
 
-function handleSchoolChange(education, onEducationChange, newSchool) {
-  const newEducation = { ...education, school: newSchool };
+function handleSchoolChange(
+  entryToUpdate,
+  education,
+  onEducationChange,
+  newSchool
+) {
+  const newEntry = { ...entryToUpdate, school: newSchool };
+  const newEducation = education.map((entry) => {
+    if (entry.id === newEntry.id) {
+      return newEntry;
+    }
+    return entry;
+  });
+
   onEducationChange(newEducation);
 }
 
-function StudyTitleInput({ education, onEducationChange }) {
+function StudyTitleInput({ entry, education, onEducationChange }) {
   return (
-    <div className="studyTitleContainerInput">
+    <div className="studyTitleInputContainer">
       <label htmlFor="studyTitle">Title of study: </label>
       <input
         className="studyTitleInput"
         type="text"
+        value={entry.title}
         placeholder="Title of study"
         id="studyTitle"
         autoComplete="off"
+        onChange={(e) =>
+          handleStudyTitleChange(
+            entry,
+            education,
+            onEducationChange,
+            e.target.value
+          )
+        }
       />
     </div>
   );
 }
 
-function StudyDateStartInput({ education, onEducationChange }) {
+function handleStudyTitleChange(
+  entryToUpdate,
+  education,
+  onEducationChange,
+  newTitle
+) {
+  const newEntry = { ...entryToUpdate, title: newTitle };
+  const newEducation = education.map((entry) => {
+    if (entry.id === newEntry.id) {
+      return newEntry;
+    }
+    return entry;
+  });
+
+  onEducationChange(newEducation);
+}
+
+function StudyDateStartInput({ entry, education, onEducationChange }) {
   return (
     <>
       <label htmlFor="studyStartDate">Start date of study: </label>
       <input
         className="studyDateStartInput"
         type="date"
+        value={entry.start}
         placeholder="Start date of study"
         id="studyStartDate"
         autoComplete="off"
+        onChange={(e) =>
+          handleStudyStartChange(
+            entry,
+            education,
+            onEducationChange,
+            e.target.value
+          )
+        }
       />
     </>
   );
 }
 
-function StudyDateEndInput({ education, onEducationChange }) {
+function handleStudyStartChange(
+  entryToUpdate,
+  education,
+  onEducationChange,
+  newStart
+) {
+  const newEntry = { ...entryToUpdate, start: newStart };
+  const newEducation = education.map((entry) => {
+    if (entry.id === newEntry.id) {
+      return newEntry;
+    }
+    return entry;
+  });
+
+  onEducationChange(newEducation);
+}
+
+function StudyDateEndInput({ entry, education, onEducationChange }) {
   return (
     <>
       <label htmlFor="studyEndDate">End date of study: </label>
       <input
         className="studyDateEndInput"
         type="date"
+        value={entry.end}
         placeholder="End date of study"
         id="studyEndDate"
         autoComplete="off"
+        onChange={(e) =>
+          handleStudyEndChange(
+            entry,
+            education,
+            onEducationChange,
+            e.target.value
+          )
+        }
       />
     </>
   );
+}
+
+function handleStudyEndChange(
+  entryToUpdate,
+  education,
+  onEducationChange,
+  newEnd
+) {
+  const newEntry = { ...entryToUpdate, end: newEnd };
+  const newEducation = education.map((entry) => {
+    if (entry.id === newEntry.id) {
+      return newEntry;
+    }
+    return entry;
+  });
+
+  onEducationChange(newEducation);
 }
 
 function EducationEntryInput(entry, education, onEducationChange) {
   return (
     <div className="educationEntryInputContainer" key={entry.id}>
       <SchoolInput
+        entry={entry}
         education={education}
         onEducationChange={onEducationChange}
       />
       <StudyTitleInput
+        entry={entry}
         education={education}
         onEducationChange={onEducationChange}
       />
       <div className="studyDateInputContainer">
         <StudyDateStartInput
+          entry={entry}
           education={education}
           onEducationChange={onEducationChange}
         />
         <StudyDateEndInput
+          entry={entry}
           education={education}
           onEducationChange={onEducationChange}
         />
--- src/components/WorkInput.jsx
+++ src/components/WorkInput.jsx
@@ -1,92 +1,199 @@
 import "../styles/WorkInput.css";
 
-function JobDescriptionInput() {
-  return (
-    <div className="jobDescriptionInputContainer">
-      <label htmlFor="jobDescription">Description of job: </label>
-      <input
-        className="jobDescriptionInput"
-        type="text"
-        placeholder="Description of job and duties performed"
-        id="jobDescription"
-        autoComplete="off"
-      />
-    </div>
-  );
-}
-
-function JobDateStartInput() {
-  return (
-    <>
-      <label htmlFor="jobStartDate">Start date of job: </label>
-      <input
-        className="jobDateStartInput"
-        type="date"
-        placeholder="Start date of job"
-        id="jobStartDate"
-        autoComplete="off"
-      />
-    </>
-  );
-}
-
-function JobDateEndInput() {
-  return (
-    <>
-      <label htmlFor="jobEndDate">End date of job: </label>
-      <input
-        className="jobDateEndInput"
-        type="date"
-        placeholder="End date of job"
-        id="jobEndDate"
-        autoComplete="off"
-      />
-    </>
-  );
-}
-
-function JobTitleInput() {
-  return (
-    <div className="jobTitleInputContainer">
-      <label htmlFor="jobTitle">Title of position: </label>
-      <input
-        className="jobTitleInput"
-        type="text"
-        placeholder="Title of position"
-        id="jobTitle"
-        autoComplete="off"
-      />
-    </div>
-  );
-}
-
-function CompanyInput() {
+function CompanyInput({ entry, work, onWorkChange }) {
   return (
     <div className="companyInputContainer">
       <label htmlFor="company">Company: </label>
       <input
         className="companyInput"
         type="text"
+        value={entry.company}
         placeholder="Company Name"
         id="company"
         autoComplete="off"
+        onChange={(e) =>
+          handleCompanyChange(entry, work, onWorkChange, e.target.value)
+        }
       />
     </div>
   );
 }
 
-function WorkInput() {
+function handleCompanyChange(entryToUpdate, work, onWorkChange, newCompany) {
+  const newEntry = { ...entryToUpdate, company: newCompany };
+  const newWork = work.map((entry) => {
+    if (entry.id === newEntry.id) {
+      return newEntry;
+    }
+    return entry;
+  });
+
+  onWorkChange(newWork);
+}
+
+function JobTitleInput({ entry, work, onWorkChange }) {
+  return (
+    <div className="jobTitleInputContainer">
+      <label htmlFor="jobTitle">Title of position: </label>
+      <input
+        className="jobTitleInput"
+        type="text"
+        value={entry.title}
+        placeholder="Title of position"
+        id="jobTitle"
+        autoComplete="off"
+        onChange={(e) =>
+          handleJobTitleChange(entry, work, onWorkChange, e.target.value)
+        }
+      />
+    </div>
+  );
+}
+
+function handleJobTitleChange(entryToUpdate, work, onWorkChange, newTitle) {
+  const newEntry = { ...entryToUpdate, title: newTitle };
+  const newWork = work.map((entry) => {
+    if (entry.id === newEntry.id) {
+      return newEntry;
+    }
+    return entry;
+  });
+
+  onWorkChange(newWork);
+}
+
+function JobDateStartInput({ entry, work, onWorkChange }) {
+  return (
+    <>
+      <label htmlFor="jobStartDate">Start date of job: </label>
+      <input
+        className="jobDateStartInput"
+        type="date"
+        value={entry.start}
+        placeholder="Start date of job"
+        id="jobStartDate"
+        autoComplete="off"
+        onChange={(e) =>
+          handleJobStartChange(entry, work, onWorkChange, e.target.value)
+        }
+      />
+    </>
+  );
+}
+
+function handleJobStartChange(entryToUpdate, work, onWorkChange, newStart) {
+  const newEntry = { ...entryToUpdate, start: newStart };
+  const newWork = work.map((entry) => {
+    if (entry.id === newEntry.id) {
+      return newEntry;
+    }
+    return entry;
+  });
+
+  onWorkChange(newWork);
+}
+
+function JobDateEndInput({ entry, work, onWorkChange }) {
+  return (
+    <>
+      <label htmlFor="jobEndDate">End date of job: </label>
+      <input
+        className="jobDateEndInput"
+        type="date"
+        value={entry.end}
+        placeholder="End date of job"
+        id="jobEndDate"
+        autoComplete="off"
+        onChange={(e) =>
+          handleJobEndChange(entry, work, onWorkChange, e.target.value)
+        }
+      />
+    </>
+  );
+}
+
+function handleJobEndChange(entryToUpdate, work, onWorkChange, newEnd) {
+  const newEntry = { ...entryToUpdate, end: newEnd };
+  const newWork = work.map((entry) => {
+    if (entry.id === newEntry.id) {
+      return newEntry;
+    }
+    return entry;
+  });
+
+  onWorkChange(newWork);
+}
+
+function JobDescriptionInput({ entry, work, onWorkChange }) {
+  return (
+    <>
+      <label htmlFor="jobDescription">Description of job: </label>
+      <input
+        className="jobDescriptionInput"
+        type="text"
+        value={entry.description}
+        placeholder="Description of job and duties performed"
+        id="jobDescription"
+        autoComplete="off"
+        onChange={(e) =>
+          handleJobDescriptionChange(entry, work, onWorkChange, e.target.value)
+        }
+      />
+    </>
+  );
+}
+
+function handleJobDescriptionChange(
+  entryToUpdate,
+  work,
+  onWorkChange,
+  newDesc
+) {
+  const newEntry = { ...entryToUpdate, description: newDesc };
+  const newWork = work.map((entry) => {
+    if (entry.id === newEntry.id) {
+      return newEntry;
+    }
+    return entry;
+  });
+
+  onWorkChange(newWork);
+}
+
+function WorkEntryInput(entry, work, onWorkChange) {
+  return (
+    <div className="workEntryInputContainer" key={entry.id}>
+      <CompanyInput entry={entry} work={work} onWorkChange={onWorkChange} />
+      <JobTitleInput entry={entry} work={work} onWorkChange={onWorkChange} />
+      <div className="jobDateInputContainer">
+        <JobDateStartInput
+          entry={entry}
+          work={work}
+          onWorkChange={onWorkChange}
+        />
+        <JobDateEndInput
+          entry={entry}
+          work={work}
+          onWorkChange={onWorkChange}
+        />
+      </div>
+      <div className="jobDescriptionInputContainer">
+        <JobDescriptionInput
+          entry={entry}
+          work={work}
+          onWorkChange={onWorkChange}
+        />
+      </div>
+    </div>
+  );
+}
+
+function WorkInput({ work, onWorkChange }) {
   return (
     <section className="workInputContainer">
       <form className="workEntryInput">
         <h2>Work experience:</h2>
-        <CompanyInput />
-        <JobTitleInput />
-        <div className="jobDateInputContainer">
-          <JobDateStartInput />
-          <JobDateEndInput />
-        </div>
-        <JobDescriptionInput />
+        {work.map((entry) => WorkEntryInput(entry, work, onWorkChange))}
       </form>
     </section>
   );
